9964ad2e9fb2a882686f069cf23adf91db59fb07
AlbertLeipzig <albertclemvill@gmail.com>
2022-04-15 15:17:04 +0200

    more info

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
-const article = require ("./article.json")
-const audioLibrary = require("./audioLibrary.json")
+const audios = require("./audios.json")
+const buttons = require("./buttons.json")
+const cards = require("./cards.json")
 const contactForm = require("./contactForm.json")
-const gallery = require("./gallery.json")
-const hero = require("./hero.json")
+const icons = require("./icons.json")
+const images = require("./images.json")
 const impressum = require("./impressum.json")
+const hero = require("./hero.json")
+const links = require("./links.json")
 const list = require("./list.json")
 const menu = require("./menu.json")
+const pickers = require("./pickers.json")
 const products = require("./products.json")
 const team = require("./team.json")
 const testData = require("./testData.json")
+const videos = require("./videos.json")
 
-export {article, audioLibrary, contactForm, gallery, hero, impressum, list, menu, products, team, testData}
+export {audios, contactForm, images, hero, impressum, list, menu, products, team, testData, buttons, pickers, videos, cards, links, icons}
