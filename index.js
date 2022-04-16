cc29f9a14fbce443b78c24cc310526d860d94890
AlbertLeipzig <albertclemvill@gmail.com>
2022-04-16 11:35:00 +0200

    updated data

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const audios = require("./audios.json")
+const buttonGroup = require("./buttonGroup.json")
 const buttons = require("./buttons.json")
 const cards = require("./cards.json")
-const contactForm = require("./contactForm.json")
+const form = require("./form.json")
 const icons = require("./icons.json")
 const images = require("./images.json")
 const impressum = require("./impressum.json")
@@ -14,5 +15,10 @@ const products = require("./products.json")
 const team = require("./team.json")
 const testData = require("./testData.json")
 const videos = require("./videos.json")
+const articles = require("./Articles/index.js")
+const slider = require("./slider.json")
+const datePicker = require("./datePicker.json")
+const colorPicker = require ("./colorPicker.json")
+const burger = require("./burger")
 
-export {audios, contactForm, images, hero, impressum, list, menu, products, team, testData, buttons, pickers, videos, cards, links, icons}
+export {audios, buttonGroup, form, images, hero, impressum, list, menu, products, team, testData, buttons, pickers, videos, cards, links, icons, articles, slider, datePicker, colorPicker, burger}
