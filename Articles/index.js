6e0d1500fa4ac1ea3454587bbb1a5fa3b7ad4fc8
AlbertLeipzig <albertclemvill@gmail.com>
2022-04-19 13:04:16 +0200

    updated dB

--- Articles/index.js
+++
@@ -1,6 +0,0 @@
-const article1 = require("./article1.json")
-const article2 = require("./article2.json")
-const article3 = require("./article3.json")
-const article4 = require("./article4.json")
-
-export {article1, article2, article3, article4}
